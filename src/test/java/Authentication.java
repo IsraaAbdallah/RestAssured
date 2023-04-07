3566b7f1a9c75e5b937cb71135b082fdc0fb4637
IsraaAbdallah <124498140+IsraaAbdallah@users.noreply.github.com>
2023-04-07 21:47:49 +0200

    Update Authentication.java

--- src/test/java/Authentication.java
+++ src/test/java/Authentication.java
@@ -6,7 +6,7 @@ public class Authentication extends Base {
     @Test
     public void testKeyAuthentication() {
 
-        RestAssured .given().baseUri("http://localhost:3000")
+        RestAssured .given().spec(request)
                 .header("key","$2a$10$b8thbYNrqewIQ/69e9rw0.G8Q9Ji.i9CPYOqBEqhYfjvbm2neweCm")
                 .when()
                 .get ("/users")
@@ -16,18 +16,18 @@ public class Authentication extends Base {
     }
     @Test
     public void testAPIKeyAuthentication () {
-        response=  request.given()
+        RestAssured .given().spec(request)
                 .queryParam ("apiKey", "$2a$10$TFsM/oiMibEYr1tKc6Ehd.Ma5WlpU83TfP/iEWVAyud9lWuGyT/3i")
                 .queryParam ("id", 3)
-                .get("/Reviews");
-        response .prettyPrint();
+                .get("/Reviews")
+                .prettyPrint();
     }
     @Test
     public void testBasicAuthentication () {
 
-      response=  request.given()
-                .auth().basic("esraa@gmail.com","abc123").get("/users/1");
-              response .prettyPrint();
+      RestAssured .given().spec(request)
+                .auth().basic("esraa@gmail.com","abc123").get("/users/1")
+                .prettyPrint();
     }
 
 }
